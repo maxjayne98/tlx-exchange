b58f105c8a037da5b6d627d7c6b11c0d1f5014e7
mahdi <mahdi.jax@gmail.com>
2022-06-28 20:04:40 +0430

    feat:add button base component

---
+++ src/components/Shared/TButton/TButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    :class="[
+      'tbutton',
+      `tbutton--${variant}`,
+      rounded && 'tbutton--rounded',
+      `${className}`,
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from "vue";
+import type { ButtonHTMLAttributes } from "vue";
+type IButtonVariants = "success" | "error" | "primary";
+export interface ITButtonProps extends ButtonHTMLAttributes {
+  label?: string;
+  variant: IButtonVariants;
+  rounded: boolean;
+  className: string;
+}
+const props = withDefaults(defineProps<ITButtonProps>(), {
+  variant: "primary",
+  rounded: true,
+});
+</script>
+
+<style scoped>
+.tbutton {
+  padding: 0.25rem 0.5rem;
+}
+.tbutton--rounded {
+  border-radius: 0.25rem;
+}
+.tbutton--success {
+  background-color: var(--color-success-500);
+}
+.tbutton--primary {
+  background-color: var(--color-primary);
+}
+.tbutton--success:hover {
+  background-color: var(--color-success-200);
+}
+
+.tbutton--error {
+  background-color: var(--color-error-500);
+}
+.tbutton--error:hover {
+  background-color: var(--color-error-200);
+}
+</style>
