5dabbe29961f10ee4066782582c22ef8509f0fb1
mahdi <mahdi.jax@gmail.com>
2022-06-28 18:44:28 +0430

    feat:add order book base component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/icons/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <OrderForm />
   </header>
 
   <main>
     <TheWelcome />
   </main>
 </template>
+
+<script setup lang="ts">
+import OrderForm from "@/components/OrderForm/OrderForm.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
 
 <style>
 @import "./assets/styles/base.css";
---
+++ src/components/OrderForm/OrderForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="order-form rounded fake-class"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.order-form {
+  background-color: var(--color-background-secondary);
+  padding: 1rem;
+}
+.fake-class {
+  height: 20rem;
+  width: 20rem;
+}
+</style>
